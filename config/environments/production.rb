8854429caf6a0946ab06007ef3e0e53180041a29
Nikhil Gupta <me@nikhgupta.com>
2018-09-01 22:18:21 +0530

    added devise views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "bit_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  # # TODO: uncomment this before deploying to production
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
